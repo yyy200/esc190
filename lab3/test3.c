ea5278794ddc0e6e26e4517e0bfe816c0cbe0a35
Youssef El Mays <youssefelmays@Youssef's-MacBook-pro.local>
2022-02-14 17:17:55 -0500

    albert is too cool

--- lab3/test3.c
+++ lab3/test3.c
@@ -10,26 +10,17 @@ int main(void){
     char ta[30] = "Albert";
     char base_item[20] = "Soup";
 
-    for(int i = 0; i < 10; i++){
+    add_request(head, base_item, 40, ta);
+    add_request(head, base_item, 90, ta);
+    add_request(head, base_item, 3000, ta);
+    add_request(head, base_item, 450, ta);
+    add_request(head, base_item, 390, ta);
+    add_request(head, base_item, 20, ta);
+    add_request(head, base_item, 2, ta);
+    add_request(head, base_item, 255, ta);
 
-        add_request(head, base_item, i*10.3,ta);
-    }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    print_ll(*head);
-    printf("########\n");
-    make_sorted(head);
-=======
->>>>>>> parent of e7225bb (I am the best coder in the world no one is above me this is a long commit messagge . OMG i am so amazing?)
     print_ll(*head);
     remove_request(head);
     printf("############\n");
     print_ll(*head);
-=======
-    print_ll(*head);
-    remove_request(head);
-    printf("############\n");
-    print_ll(*head);
->>>>>>> parent of e7225bb (I am the best coder in the world no one is above me this is a long commit messagge . OMG i am so amazing?)
 }
